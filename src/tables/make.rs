e632dbd6a0da16756a85cd080bd179c57416008d
scavanagh <scavanagh@ATGLT003>
2026-08-04 08:43:02 -0500

    class structure

--- src/tables/make.rs
+++ src/tables/make.rs
@@ -1,8 +1,9 @@
-use std::any::Any;
+use std::{any::{Any, TypeId}, collections::HashMap};
 
 use glam::{Vec3, prelude::Vec4};
+use slotmap::{SlotMap, new_key_type};
 
-use crate::{spacial::transform::Transform, tables::class::Class};
+use crate::{spacial::transform::Transform, tables::{class::Class, class_strategy::rarity}};
 
 #[derive(Default, Debug)]
 pub struct Color {
@@ -17,29 +18,36 @@ impl Color {
 struct FishContext {
     pub color: f32,
 }
-impl Make<TableView> for FishContext {
-    fn make_into<'a>(&mut self, view: &'a mut TableView) -> &'a mut TableView {
-        view.xform(|xform| xform.xyz(Vec3::Z))
+impl FishContext {
+    pub fn make_into(core: &mut CoreAddition, additions: AdditionsView) {
+        
     }
 }
-impl Make<TableViewColor> for FishContext {
-    fn make_into<'a>(&mut self, view: &'a mut TableViewColor) -> &'a mut TableViewColor {
-        view.color(|color| color.rgba(Vec4::ZERO))
+new_key_type! {
+    pub struct TableIndirectionId;
+}
+struct CoreAddition {
+    pub xforms: Class<Transform>,
+    pub heirarchy: Class<Transform>,
+    pub names: Class<String>,
+}
+pub trait Addition: Any {}
+impl Addition for CoreAddition {}
+pub struct Domain {
+    core: CoreAddition,
+    additions: HashMap<TypeId, Box<dyn Addition>>,
+}
+pub struct AdditionsView<'domain> {
+    additions: &'domain mut HashMap<TypeId, Box<dyn Addition>>,
+}
+
+impl<'domain> AdditionsView<'domain> {
+    pub fn get_addition<T: Addition + 'static>(&mut self) -> Option<&mut T> {
+        let id = TypeId::of::<T>();
+        self.additions
+            .get_mut(&id)
+            .and_then(|any| (any.as_mut() as &mut dyn Any).downcast_mut::<T>())
     }
-}
-
-pub struct Tables {
-    pub tables: Vec<dyn Viewable>,
-}
-
-
-pub trait Make<T>: Any {
-    fn make_into<'a>(&mut self, view: &'a mut T) -> &'a mut T;
-}
-
-pub trait Viewable {
-    type Out;
-    fn view(&mut self) -> Self::Out;
 }
 pub struct TableMain {
     xforms: Class<Transform>,
@@ -47,20 +55,14 @@ pub struct TableMain {
 pub struct TableView {
     xform: Option<Transform>,
 }
-impl Viewable for TableMain {
-    type Out = TableView;
+impl TableView {
+    
+}
+struct World {
+    view: TableView,
+    color: TableViewColor,
+}
 
-    fn view(&mut self) -> Self::Out {
-        todo!()
-    }
-} 
-impl Viewable for TableColor {
-    type Out = TableViewColor;
-
-    fn view(&mut self) -> Self::Out {
-        todo!()
-    }
-} 
 impl TableView {
     pub fn xform<F: FnOnce(&mut Transform) -> &mut Transform>(&mut self, f: F) -> &mut Self {
         let xform = self.xform.get_or_insert_with(Transform::default);
@@ -82,4 +84,5 @@ impl TableViewColor {
         self
     }
 }
+
 
