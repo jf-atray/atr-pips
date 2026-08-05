0cde4c0d120936106880faaf2940d8aea420e3e4
scavanagh <scavanagh@ATGLT003>
2026-08-05 16:38:58 -0500

    its coming home hold on

--- src/gpuscope/canvasing/mod.rs
+++ src/gpuscope/canvasing/mod.rs
@@ -1,5 +1,28 @@
+use slotmap::{SlotMap, SparseSecondaryMap, new_key_type};
 use wgpu::{Device, Queue, RenderPass};
 
+new_key_type! {
+    pub struct CanvasId;
+    pub struct MaterialId;
+}
+
+// canvas ID probably just points to solid resources
+// all the "instance download" and "calling things" logic _belongs_ to the solver
+pub struct SimpleCanvasDesign {
+    
+}
+pub struct SimpleCanvasSolver {
+    canvases: SparseSecondaryMap<CanvasId, SimpleCanvasDesign>,
+}
+pub struct ComplexCanvasDesign {
+
+}
+pub struct ComplexCanvasSolver {
+    canvases: SparseSecondaryMap<CanvasId, ComplexCanvasDesign>,
+}
+pub struct Canvasing {
+    canvases: SlotMap<CanvasId, EveryCanvas>,
+}
 pub struct EveryCanvas {
     pub bind: wgpu::RenderPipeline,
     pub layout: wgpu::PipelineLayout,
