6c5069c5b205d468e8a3f80947961d32094b0f37
scavanagh <scavanagh@ATGLT003>
2026-08-26 12:46:48 -0500

    works but lets use a real crate instead of rolling our own casting

--- src/addition/mod.rs
+++ src/addition/mod.rs
@@ -10,17 +10,72 @@ struct ExampleDomain {
     pub signals: TypedMap<dyn Signals>,
 }
 
-
+//todo I don't like this. Check nightly or some standard std impl
+trait AsAny : Any {
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
 struct TypedMap<T: ?Sized>(HashMap<TypeId, Box<T>>);
-impl<T: Any> TypedMap<T> {
-    pub fn get<K: 'static, U: 'static>(&mut self) -> Option<&mut U> {
+impl<T: ?Sized> TypedMap<T> {
+    pub fn insert<K: 'static>(&mut self, t: Box<T>) {
         let id = TypeId::of::<K>();
-        let tables = self.0.get_mut(&id) ?;
-        let tables = tables.as_mut();
-        let tables_any = tables as &mut dyn Any;
-        tables_any.downcast_mut::<U>()
+        self.0.insert(id, t);
     }
 }
+impl<T: ?Sized> TypedMap<T>
+where T: Tables+AsAny {
+    pub fn get_tables<K: Addition+'static>(&mut self) -> Option<&mut K::Tables> {
+        let id = TypeId::of::<K>();
+        let tables = self.0.get_mut(&id) ?;
+        
+        let tables = tables.as_mut();
+        let tables_any = tables.as_any_mut();
+        tables_any.downcast_mut::<K::Tables>()
+    }
+}
+impl<T: ?Sized> TypedMap<T>
+where T: Solvers+AsAny {
+    pub fn get_solvers<K: Addition+'static>(&mut self) -> Option<&mut K::Solvers> {
+        let id = TypeId::of::<K>();
+        let tables = self.0.get_mut(&id) ?;
+        
+        let tables = tables.as_mut();
+        let tables_any = tables.as_any_mut();
+        tables_any.downcast_mut::<K::Solvers>()
+    }
+}
+impl<T: ?Sized> TypedMap<T>
+where T: Scripts+AsAny {
+    pub fn get_scripts<K: Addition+'static>(&mut self) -> Option<&mut K::Scripts> {
+        let id = TypeId::of::<K>();
+        let tables = self.0.get_mut(&id) ?;
+        
+        let tables = tables.as_mut();
+        let tables_any = tables.as_any_mut();
+        tables_any.downcast_mut::<K::Scripts>()
+    }
+}
+impl<T: ?Sized> TypedMap<T>
+where T: Signals+AsAny {
+    pub fn get_signals<K: Addition+'static>(&mut self) -> Option<&mut K::Signals> {
+        let id = TypeId::of::<K>();
+        let tables = self.0.get_mut(&id) ?;
+        
+        let tables = tables.as_mut();
+        let tables_any = tables.as_any_mut();
+        tables_any.downcast_mut::<K::Signals>()
+    }
+}
+impl<T:Any> AsAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self as &dyn Any
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self as &mut dyn Any
+    }
+}
+
 impl<T: ?Sized> Default for TypedMap<T> {
     fn default() -> Self {
         Self(HashMap::default())
@@ -32,58 +87,31 @@ impl<T: ?Sized> Debug for TypedMap<T> {
     }
 }
 impl ExampleDomain {
-    pub fn get_tables<T: Addition+'static>(&mut self) -> Option<&mut T::Tables> {
-        self.tables.
-    }
     pub fn get<T: Addition+'static>(&mut self) -> Option<AsViewMut<'_, T>> {
-        let id = TypeId::of::<T>();
-        let tables = self.tables.get_mut(&id) ?;
-        let solvers = self.solvers.get_mut(&id) ?;
-        let scripts = self.scripts.get_mut(&id) ?;
-        let signals = self.signals.get_mut(&id) ?;
-
-        let tables = tables.as_mut();
-        let tables_any = tables as &mut dyn Any;
-        let tables = tables_any.downcast_mut::<T::Tables>() ?;
-
-        let solvers = solvers.as_mut();
-        let tables_any = solvers as &mut dyn Any;
-        let solvers = tables_any.downcast_mut::<T::Solvers>() ?;
-
-        let scripts = scripts.as_mut();
-        let tables_any = scripts as &mut dyn Any;
-        let scripts = tables_any.downcast_mut::<T::Scripts>() ?;
-
-        let signals = signals.as_mut();
-        let tables_any = signals as &mut dyn Any;
-        let signals = tables_any.downcast_mut::<T::Signals>() ?;
+        let tables = self.tables.get_tables::<T>() ?;
+        let solvers = self.solvers.get_solvers::<T>() ?;
+        let scripts = self.scripts.get_scripts::<T>() ?;
+        let signals = self.signals.get_signals::<T>() ?;
 
         let view = AsViewMut::<T>::new(tables, solvers, scripts, signals);
         Some(view)
     }
     pub fn add<T: Addition + 'static>(&mut self) -> Result<AsViewMut<'_,T>, ()> {
-        let id = TypeId::of::<T>();
-
-        //todo, make this a nice trait call
         let tables = T::make_tables();
         let boxed = Box::new(tables);
-        let tables = boxed as Box<dyn Tables>;
-        self.tables.insert(id, tables);
+        self.tables.insert::<T>(boxed);
 
         let solvers = T::make_solvers();
         let boxed = Box::new(solvers);
-        let solvers = boxed as Box<dyn Solvers>;
-        self.solvers.insert(id, solvers);
+        self.solvers.insert::<T>(boxed);
 
         let scripts = T::make_scripts();
         let boxed = Box::new(scripts);
-        let scripts = boxed as Box<dyn Scripts>;
-        self.scripts.insert(id, scripts);
+        self.scripts.insert::<T>(boxed);
 
         let signals = T::make_signals();
         let boxed = Box::new(signals);
-        let signals = boxed as Box<dyn Signals>;
-        self.signals.insert(id, signals);
+        self.signals.insert::<T>(boxed);
 
         //todo, make a helper impl over option and result
         self.get::<T>().ok_or_else(|| {
@@ -128,13 +156,13 @@ where
 
     }
 }
-trait Tables: Any {}
-trait Solvers: Any {}
-trait Solver: Any {}
+trait Tables: AsAny+Any {}
+trait Solvers: AsAny+Any {}
+trait Solver: AsAny+Any {}
 impl<T: Solver> Solvers for T {}
 
-trait Scripts: Any {}
-trait Signals: Any {}
+trait Scripts: AsAny+Any {}
+trait Signals: AsAny+Any {}
 type AsViewMut<'a, A>
         = ViewMut<'a, <A as Addition>::Tables, <A as Addition>::Solvers, <A as Addition>::Scripts, <A as Addition>::Signals>;
 
@@ -197,18 +225,13 @@ mod test {
     }
     #[test]
     pub fn creation() {
-        let mut domain = ExampleDomain {
-            tables: HashMap::new(),
-            solvers: HashMap::new(),
-            scripts: HashMap::new(),
-            signals: HashMap::new(),
-        };
+        let mut domain = ExampleDomain::default();
 
         let rslt = domain.add::<CowboyWorld>();
         println!("{rslt:#?}");
         assert_matches!(rslt, Ok(_));
 
-        let cowboy_tables = domain.get_tables::<CowboyWorld>()
+        let cowboy_tables = domain.tables.get_tables::<CowboyWorld>()
             .expect("Expect cowboyworld to exist by now.");
     }
 }
