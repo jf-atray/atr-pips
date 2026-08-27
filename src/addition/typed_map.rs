a0649e06da6b098ed7bfb8e76b0435894f99ce2d
scavanagh <scavanagh@ATGLT003>
2026-08-27 12:35:52 -0500

    always-on generic

--- src/addition/typed_map.rs
+++ src/addition/typed_map.rs
@@ -2,38 +2,44 @@ use std::{any::TypeId, collections::HashMap, fmt::Debug};
 
 use downcast_rs::Downcast;
 
-pub struct TypedMap<T: ?Sized>(HashMap<TypeId, Box<T>>);
+pub struct TypedMap<T: ?Sized, K: T> {
+    pub core: K,
+    rest: HashMap<TypeId, Box<T>>,
+}
 
-impl<T: ?Sized> TypedMap<T> {
-    //so sorry but people keep reaching into here. todo, make a wrapper struct for locked visibility
-    pub(super) fn insert<K: 'static>(&mut self, value: Box<T>) {
-        let id = TypeId::of::<K>();
-        self.0.insert(id, value);
+impl<T: ?Sized, K: T> TypedMap<T, K> {
+    pub fn new(core: K) -> Self {
+        Self {
+            core,
+            rest: HashMap::new(),
+        }
+    }
+
+    pub(super) fn insert<L: 'static>(&mut self, value: Box<T>) {
+        let id = TypeId::of::<L>();
+        self.rest.insert(id, value);
     }
 }
 
-impl<T: ?Sized + Downcast> TypedMap<T> {
-    pub fn get_mut<K: 'static, U: 'static>(&mut self) -> Option<&mut U> {
-        let id = TypeId::of::<K>();
-        let value = self.0.get_mut(&id)?;
+impl<T: ?Sized + Downcast, K: T> TypedMap<T, K> {
+    pub fn get_mut<L: 'static, U: 'static>(&mut self) -> Option<&mut U> {
+        let id = TypeId::of::<L>();
+        let value = self.rest.get_mut(&id)?;
         let inner = value.as_mut();
         inner.as_any_mut().downcast_mut::<U>()
     }
 
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
-        self.0.values_mut().map(|b| b.as_mut())
+        let one = std::iter::once(&mut self.core);
+        one.chain(self.rest.values_mut().map(|b| b.as_mut()))
     }
 }
 
-
-impl<T: ?Sized> Default for TypedMap<T> {
-    fn default() -> Self {
-        Self(HashMap::default())
-    }
-}
-
-impl<T: ?Sized> Debug for TypedMap<T> {
+impl<T: ?Sized + Debug, K: T + Debug> Debug for TypedMap<T, K> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("TypedMap").finish()
+        f.debug_struct("TypedMap")
+            .field("core", &self.core)
+            .field("rest", &self.rest.len())
+            .finish()
     }
 }
